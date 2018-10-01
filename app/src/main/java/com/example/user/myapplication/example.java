5ae3ce2717ef8eccb26d8acdf86b5425e774a826
joy97q12@naver.com <joy9q712@naver.com>
2018-10-01 17:21:13 +0900

    AndroidStudio 18.10.01

---
+++ app/src/main/java/com/example/user/myapplication/example.java
@@ -0,0 +1,42 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class example extends AppCompatActivity {
+
+    private Button calbtn, playerbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+
+        init();
+    }
+    private void init() {
+        calbtn = findViewById(R.id.calcBtn);
+        calbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(example.this, "계산기", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        playerbtn = findViewById(R.id.playerBtn);
+        playerbtn.setOnClickListener(new Button2());
+
+    }
+
+    class Button2 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(example.this, "플레이어", Toast.LENGTH_LONG).show();
+
+        }
+    }
+}
