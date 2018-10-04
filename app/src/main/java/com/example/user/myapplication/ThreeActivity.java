412e3a4c5966356bae0256debb7da6dd67ceea10
joy97q12@naver.com <joy9q712@naver.com>
2018-10-04 17:17:48 +0900

    AndroidStudio 18.10.01

--- app/src/main/java/com/example/user/myapplication/ThreeActivity.java
+++ app/src/main/java/com/example/user/myapplication/ThreeActivity.java
@@ -1,13 +1,88 @@
 package com.example.user.myapplication;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ThreeActivity extends AppCompatActivity {
+
+    private Button pause;
+    private Button stop;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+        init();
+    }
+
+    private void init() {
+        setTitle(R.string.mp3Title);
+        pause = findViewById(R.id.pause);
+        stop = findViewById(R.id.stop);
+
+    }
+
+
+    public void clkButtons(View v) {
+        if (v instanceof Button) {
+            Button b = (Button)v;
+            switch (b.getId()) {
+                case R.id.song1 :
+                    _doStop();
+                    Toast.makeText(ThreeActivity.this, "노래1번 재생", Toast.LENGTH_SHORT).show();
+                    mp = MediaPlayer.create(ThreeActivity.this,R.raw.song1);
+                    mp.start();
+                    break;
+
+                case R.id.song2 :
+                    _doStop();
+                    Toast.makeText(ThreeActivity.this, "노래2번 재생", Toast.LENGTH_SHORT).show();
+                    mp = MediaPlayer.create(ThreeActivity.this,R.raw.song2);
+                    mp.start();
+                    break;
+
+                case R.id.pause :
+                    if(mp == null) {
+                        Toast.makeText(ThreeActivity.this, "재생중인 노래 없음", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Button pause = (Button)v;
+                    String txt = pause.getText().toString();
+                    if(txt.equals(getString(R.string.pause))) {
+                        if(mp.isPlaying()) {
+                            Toast.makeText(ThreeActivity.this, "일시정지", Toast.LENGTH_SHORT).show();
+                            mp.pause();
+                            pause.setText(R.string.play);
+                        }
+
+                    } else if(txt.equals("재생")) {
+                        Toast.makeText(ThreeActivity.this, "재생", Toast.LENGTH_SHORT).show();
+                        mp.start();
+                        pause.setText(R.string.pause);
+                    }
+                    break;
+
+                case R.id.stop :
+                    _doStop();
+                    break;
+            }
+        }
+    }
+
+    private void _doStop() {
+        String txt = pause.getText().toString();
+        if(mp != null) {
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
+        if(txt.equals(getString(R.string.play))) {
+            pause.setText(R.string.pause);
+        }
     }
 }
--- app/src/main/java/com/example/user/myapplication/TwoActivity.java
+++ app/src/main/java/com/example/user/myapplication/TwoActivity.java
@@ -3,6 +3,7 @@ package com.example.user.myapplication;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,6 +29,7 @@ public class TwoActivity extends AppCompatActivity {
     }
 
     private void init() {
+        setTitle(R.string.calcTitle);
         getSupportActionBar().setTitle("계산기");
         firstet = findViewById(R.id.firstet);
         firstet.setShowSoftInputOnFocus(false);
@@ -35,7 +37,7 @@ public class TwoActivity extends AppCompatActivity {
         secondet.setShowSoftInputOnFocus(false);
         firsttv = findViewById(R.id.firsttv);
         secondtv = findViewById(R.id.secondtv);
-        calcbtnResult = findViewById(R.id.calcbtnResult);
+        calcbtnResult = findViewById(R.id.btnResult);
 
     }
 
@@ -60,8 +62,86 @@ public class TwoActivity extends AppCompatActivity {
         }
     }
 
+    private int intParseFromString(String v) {
+        int result = -1;
+        try {
+            result = Integer.parseInt(v);
+        } catch(Exception e) {
+            return result;
+        }
+        return result;
+    }
+
+    private String doCalc(String v1, String v2, String symbol) {
+        String result = "";
+        int intV1 = intParseFromString(v1);
+        int intV2 = intParseFromString(v2);
+
+        switch (symbol) {
+            case "+" :
+                result = Integer.toString(intV1 + intV2);
+                break;
+            case "-" :
+                result = Integer.toString(intV1 - intV2);
+                break;
+            case "/" :
+                if(intV2 == 0) {
+                    result = "DIV0";
+                } else {
+                    result = Double.toString((double)intV1 / intV2);
+                }
+
+                break;
+            case "*" :
+                result = Integer.toString(intV1 * intV2);
+                break;
+        }
+        return(result);
+    }
+    //숫자 버튼 이벤트
+    public void clkNoBtn(View v) {
+        Log.i("test", "이벤트 연결 성공");
+        String calcSymbol = firsttv.getText().toString();
+
+        if (v.getId() == R.id.btnResult) {
+            String val1 = firstet.getText().toString();
+            String val2 = secondet.getText().toString();
+            if(calcSymbol.equals("") || val1.equals("") || val2.equals("")) {
+                secondtv.setText("incomplete");
+            } else {
+                String result = doCalc(val1, val2, calcSymbol);
+                secondtv.setText(result);
+            }
+        } else {
+            EditText target = null;
+            if (calcSymbol.equals("")) {
+                target = firstet;
+            } else {
+                target = secondet;
+                // secondet.requestFocus();
+            }
+
+            if (target != null) {
+
+                if (v instanceof Button) {
+                    //누른 버튼의 텍스트 값
+                    Button b = (Button) v;
+                    String pressNo = b.getText().toString();
+
+                    //기존에 있던 텍스트 값 + 누른 버튼의 텍스트 값
+                    String existValue = target.getText().toString();
+                    existValue += pressNo;
+
+                    //합친 텍스트 값을 다시 타겟에 입력
+                    target.setText(existValue);
+                }
+            }
+        }
+    }
+
 
     public void clkLeftBtn(View v) {
+        Log.i("test","이벤트 연결 성공");
         if(v instanceof Button) {
             Button b = (Button)v;
             if(b.getText().toString() != calcbtnResult.getText().toString()) {
@@ -73,23 +153,36 @@ public class TwoActivity extends AppCompatActivity {
                     secondet.setText(str2);
                 }
             } else {
-                int n1 = Integer.parseInt(str);
-                int n2 = Integer.parseInt(str2);
-                int result;
-                if(firsttv.getText().toString().equals("+")) {
+                if(firstet.getText().toString().equals("")) {
+                    secondtv.setText("incomplete");
+                } else if(secondet.getText().toString().equals("")) {
+                    secondtv.setText("incomplete");
+                } else if(firstet.getText().toString().equals("") && secondet.getText().toString().equals("")) {
+                    secondtv.setText("incomplete");
+                }  else {
+                    int n1 = Integer.parseInt(str);
+                    int n2 = Integer.parseInt(str2);
+                    int result;
+                    if (firsttv.getText().toString().equals("+")) {
                         result = n1 + n2;
                         secondtv.setText("" + result);
-                } else if (firsttv.getText().toString().equals("-")) {
-                    result = n1 - n2;
-                    secondtv.setText(""+ result);
-                } else if (firsttv.getText().toString().equals("/")) {
-                    Double d1 = Double.parseDouble(str);
-                    Double d2 = Double.parseDouble(str2);
+                    } else if (firsttv.getText().toString().equals("-")) {
+                        result = n1 - n2;
+                        secondtv.setText("" + result);
+                    } else if (firsttv.getText().toString().equals("/")) {
+                        Double d1 = Double.parseDouble(str);
+                        Double d2 = Double.parseDouble(str2);
+                        if(firstet.getText().toString().equals("0") || secondet.getText().toString().equals("0")) {
+                            secondtv.setText("DIV0");
+                        } else {
+                            Double divResult = Double.parseDouble(String.format("%.3f",(d1/d2)));
 
-                    secondtv.setText(""+ (d1 / d2));
-                } else {
-                    result = n1 * n2;
-                    secondtv.setText(""+ result);
+                            secondtv.setText(""+divResult);
+                        }
+                    } else {
+                        result = n1 * n2;
+                        secondtv.setText("" + result);
+                    }
                 }
             }
         }
